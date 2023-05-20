3311408404bb3545cfaf928deb039390ef43f148
eligazza <gazzaelias@gmail.com>
2023-05-20 03:23:14 -0300

    clase 12 terminada con logs y todo

--- 12 Integradora/src/db/H2db.java
+++ 12 Integradora/src/db/H2db.java
@@ -1,11 +1,16 @@
 package db;
 
 import modelos.Usuario;
+import org.apache.log4j.Logger;
 
 import java.sql.*;
 
 public class H2db {
 
+    // ATRIBUTOS
+    private static final Logger LOG = Logger.getLogger(H2db.class);
+
+    // METODOS
     public Connection creoConexion(){
 
         // Importo la clase de h2
@@ -17,6 +22,7 @@ public class H2db {
 
         // Establezco la conexion
         try {
+            LOG.info("Conexión con la base de datos establecida.");
             return DriverManager.getConnection("jdbc:h2:~/usuarios;PASSWORD=sa;USER=sa"); // retorno la conexión
         } catch (SQLException e) {
             System.out.println("No se pudo establecer la conexión");
@@ -41,9 +47,10 @@ public class H2db {
 
             Statement statementCreate = connection.createStatement();
             statementCreate.execute(queryCreate);
+            LOG.info("Tabla de datos 'Usuarios' creada con éxito.");
 
         } catch (SQLException e) {
-            System.out.println("No se pudo crear la tabla");
+            LOG.error("No se pudo crear la tabla (SQLException)");
             throw new RuntimeException(e);
         }
 
@@ -60,9 +67,10 @@ public class H2db {
                     "'," + usuario.getEdad() + ")";
             Statement statementInsert = connection.createStatement();
             statementInsert.execute(queryInsert);
+            LOG.info("El usuario <" + usuario.getApellido() + ", " + usuario.getNombre() + "> ha sido registrado");
 
         } catch (SQLException e) {
-            System.out.println("Error al insertar usuario");
+            LOG.error("Error al insertar usuario, intente de nuevo (SQLException)");
             throw new RuntimeException(e);
         }
     }
@@ -71,12 +79,21 @@ public class H2db {
 
         // Elimino un usuario según el id
         try{
-            String queryDelete = "DELETE FROM USUARIOS where ID = '" + id + "'";
+            String userInfo = "SELECT APELLIDO, NOMBRE from USUARIOS where ID = "+ id;
+            Statement statementInfo = connection.createStatement();
+            ResultSet usuario = statementInfo.executeQuery(userInfo);
+            while (usuario.next()) {
+                LOG.warn("El usuario <" +
+                        usuario.getString(1) + ", " +
+                        usuario.getString(2)+ "> ha sido eliminado");
+            }
+
+            String queryDelete = "DELETE from USUARIOS where ID = '" + id + "'";
             Statement statementDelete = connection.createStatement();
             statementDelete.execute(queryDelete);
 
         } catch (SQLException e) {
-            System.out.println("Error al borrar registro");
+            LOG.error("Error al borrar registro (SQLException)");
             throw new RuntimeException(e);
         }
     }
@@ -90,13 +107,13 @@ public class H2db {
             ResultSet resultado = statementSelect.executeQuery(selectAll); // Acá se pone executeQuery porque no espera booleano, sino un dataset
             while (resultado.next()) {
                 System.out.println("ID: " + resultado.getInt(1) +
-                        ", Nombre:" + resultado.getString(2) +
+                        ", Nombre: " + resultado.getString(2) +
                         ", Apellido: " + resultado.getString(3) +
                         ", Edad: " + resultado.getInt(4));
             }
 
         } catch (SQLException e) {
-            System.out.println("Error al imprimir tabla");
+            LOG.error("Error al imprimir tabla (SQLException)");
             throw new RuntimeException(e);
         }
     }
